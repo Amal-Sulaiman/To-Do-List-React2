8204f188f4454324905b36208428800071d2921d
Amal-Sulaiman <129241342+Amal-Sulaiman@users.noreply.github.com>
2025-06-24 09:40:45 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import "./App.css";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return <TodoList />;
+}
+
+export default App;
